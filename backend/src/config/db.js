ae616f4a8157f07b5d3a5a946dfbf0664110005b
Zeevesh Shaklan <happyshaklan10499@gmail.com>
2025-10-19 20:52:11 +0530

    database connectivity issue

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,29 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if (cached.conn) {
+      return cached.conn;
+    }
+
+    if (!cached.promise) {
+      cached.promise = mongoose
+        .connect(process.env.MONGODB_URI, {
+          serverSelectionTimeoutMS: 30000,
+          bufferCommands: false,
+        })
+        .then((mongoose) => mongoose);
+    }
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
+    cached.conn = await cached.promise;
+    return cached.conn;
   } catch (error) {
     console.log("MongoDB connection error :", error.message);
     process.exit(1);
